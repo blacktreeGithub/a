d884f473261c3ccc15b17f165f0e6f0b0be4a5bc
blacktreeGithub <shunta.kuroki@gmail.com>
2024-10-18 19:00:09 +0900

    Create que.tsx

---
+++ que.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'; import { Inquiry } from 'backend/generated-rest-client'; import { InquiryList } from 'components/parts/PostList/PostList'; import { InquiryService } from 'services/InquiryService'; import styles from './Home.module.css';
+
+export const HomePage = () => {
+
+const [inquiries, setInquiries] = useState<Inquiry[]>([]); const [sortAscend, setSortAscend] = useState(false);
+
+useEffect(() => {
+
+const loadInquiries = async () => {
+  const inquiries = await InquiryService.getInquiries();
+  setInquiries(inquiries);
+};
+loadInquiries();
+}, []);
+
+useEffect(() => {
+
+const sortedInquiries = [...inquiries].sort((first, second) => {
+  if (sortAscend) {
+    return new Date(first.datetime).getTime() - new Date(second.datetime).getTime();
+  } else {
+    return new Date(second.datetime).getTime() - new Date(first.datetime).getTime();
+  }
+});
+setInquiries(sortedInquiries);
+}, [sortAscend]);
+
+let inquiryCount; if (inquiries.length > 9999) { // 問
